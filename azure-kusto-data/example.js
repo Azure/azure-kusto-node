5fbf371653e75ea34695515bf0e670933c56a5b4
Daniel Dubovski <dadubovs@microsoft.com>
2018-12-17 16:04:15 +0200

    Adding example of using ClientRequestProperties (#11)
    * Adding example of using ClientRequestProperties
    
    * update docs

--- azure-kusto-data/example.js
+++ azure-kusto-data/example.js
@@ -1,5 +1,6 @@
 const KustoClient = require("azure-kusto-data").Client;
 const KustoConnectionStringBuilder = require("azure-kusto-data").KustoConnectionStringBuilder;
+const ClientRequestProperties = require("azure-kusto-data").ClientRequestProperties;
 
 let clusterName = "";
 let username = "username";
@@ -13,3 +14,21 @@ kustoClient.execute("db", "TableName | limit 1", (err, results) => {
     console.log(JSON.stringify(results));
     console.log(results.primaryResults[0].toString());
 });
+
+// providing ClientRequestProperties
+// for a complete list of ClientRequestProperties
+// go to https://docs.microsoft.com/en-us/azure/kusto/api/netfx/request-properties#list-of-clientrequestproperties
+let clientRequestProps = new ClientRequestProperties();
+const ONE_MINUTE = 1000 * 60;
+clientRequestProps.setOption("servertimeout", ONE_MINUTE);
+
+kustoClient.execute(
+    "db", 
+    "TableName | limit 1", 
+    (err, results) => {
+        if (err) throw new Error(err);
+        console.log(JSON.stringify(results));
+        console.log(results.primaryResults[0].toString());
+    }, 
+    clientRequestProps
+);
