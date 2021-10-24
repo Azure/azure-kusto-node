2e4fc331ceaeb0cc1c6ffcb37bd3ed497ee80b19
Ohad Bitton <ohbitton@microsoft.com>
2021-10-24 19:59:15 +0300

    f

--- azure-kusto-ingest/source/managedStreamingIngestClient.ts
+++ azure-kusto-ingest/source/managedStreamingIngestClient.ts
@@ -11,8 +11,8 @@ import {KustoResponseDataSet, sleep} from "azure-kusto-data/source/response";
 import StreamingIngestClient from "./streamingIngestClient";
 import IngestClient from "./ingestClient";
 import { QueueSendMessageResponse } from "@azure/storage-queue";
-const streamify = require('stream-array');
-const toArray = require('stream-to-array');
+import streamify = require("stream-array");
+import toArray = require("stream-to-array");
 
 const maxSteamSize = 1024 * 1024 * 4;
 const maxRetries = 3
