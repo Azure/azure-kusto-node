7e7082ded7a51295f45ee55801a39d01372829f9
AsafMah <asafmahlev@microsoft.com>
2021-11-18 09:25:48 +0200

    Small fixes - expand streaming ingest to all Readable, fixed some imports, typos and updated gitignore (#153)
    * Small fixes - fixed type and updated gitignore
    
    * Expand IngestFromStream to any Readable, and fix some import errors

--- azure-kusto-ingest/source/streamingIngestClient.ts
+++ azure-kusto-ingest/source/streamingIngestClient.ts
@@ -5,11 +5,11 @@ import IngestionProperties from "./ingestionProperties";
 
 import {CompressionType, FileDescriptor, StreamDescriptor} from "./descriptors";
 import zlib from "zlib";
-import fs from "fs";
 import {AbstractKustoClient} from "./abstractKustoClient";
 import {Client as KustoClient, KustoConnectionStringBuilder} from "azure-kusto-data";
 import {KustoResponseDataSet} from "azure-kusto-data/source/response";
 import { fileToStream } from "./utils";
+import { Readable } from "stream";
 
 class KustoStreamingIngestClient extends AbstractKustoClient {
     private kustoClient: KustoClient;
@@ -20,7 +20,7 @@ class KustoStreamingIngestClient extends AbstractKustoClient {
         this.kustoClient = new KustoClient(kcsb);
     }
 
-    async ingestFromStream(stream: StreamDescriptor | fs.ReadStream, ingestionProperties: IngestionProperties): Promise<KustoResponseDataSet> {
+    async ingestFromStream(stream: StreamDescriptor | Readable, ingestionProperties: IngestionProperties): Promise<any> {
         const props = this._mergeProps(ingestionProperties);
         props.validate();
         const descriptor: StreamDescriptor = stream instanceof StreamDescriptor ? stream : new StreamDescriptor(stream);
