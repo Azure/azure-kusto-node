78a1708ea1c01159969a0dc287b28fbe7b4955c0
Asaf Mahlev <asafmahlev@microsoft.com>
2021-12-14 09:12:03 +0200

    Added helper methods + fixed default props not propegating

--- azure-kusto-ingest/source/managedStreamingIngestClient.ts
+++ azure-kusto-ingest/source/managedStreamingIngestClient.ts
@@ -18,6 +18,7 @@ import { ExponentialRetry } from "./retry";
 
 const maxStreamSize = 1024 * 1024 * 4;
 const attemptCount = 3
+const ingestPrefix = "https://ingest-";
 
 class KustoManagedStreamingIngestClient extends AbstractKustoClient {
     private streamingIngestClient: StreamingIngestClient;
@@ -25,10 +26,46 @@ class KustoManagedStreamingIngestClient extends AbstractKustoClient {
     private baseSleepTimeSecs = 1;
     private baseJitterSecs = 1;
 
+    /**
+     * Creates a KustoManagedStreamingIngestClient from a DM connection string.
+     * This method infers the engine connection string.
+     * For advanced usage, use the constructor that takes a DM connection string and an engine connection string.
+     * @param dmConnectionString The DM connection string.
+     * @param defaultProps The default ingestion properties.
+     */
+    static fromDmConnectionString(dmConnectionString: KustoConnectionStringBuilder, defaultProps: IngestionProperties | null = null): KustoManagedStreamingIngestClient {
+        if (dmConnectionString.dataSource == null || !dmConnectionString.dataSource.startsWith(ingestPrefix)) {
+            throw new Error(`DM connection string must include the prefix '${ingestPrefix}'`);
+        }
+
+        const engineConnectionString = KustoConnectionStringBuilder.fromExisting(dmConnectionString);
+        engineConnectionString.dataSource = engineConnectionString.dataSource?.replace(ingestPrefix, "https://");
+
+        return new KustoManagedStreamingIngestClient(engineConnectionString, dmConnectionString, defaultProps);
+    }
+
+    /**
+     * Creates a KustoManagedStreamingIngestClient from a engine connection string.
+     * This method infers the engine connection string.
+     * For advanced usage, use the constructor that takes an engine connection string and an engine connection string.
+     * @param engineConnectionString The engine connection string.
+     * @param defaultProps The default ingestion properties.
+     */
+    static fromEngineConnectionString(engineConnectionString: KustoConnectionStringBuilder, defaultProps: IngestionProperties | null = null): KustoManagedStreamingIngestClient {
+        if (engineConnectionString.dataSource == null || engineConnectionString.dataSource.startsWith(ingestPrefix)) {
+            throw new Error(`Engine connection string must not include the prefix '${ingestPrefix}'`);
+        }
+
+        const dmConnectionString = KustoConnectionStringBuilder.fromExisting(engineConnectionString);
+        dmConnectionString.dataSource = dmConnectionString.dataSource?.replace("https://", ingestPrefix);
+
+        return new KustoManagedStreamingIngestClient(engineConnectionString, dmConnectionString, defaultProps);
+    }
+
     constructor(engineKcsb: string | KustoConnectionStringBuilder, dmKcsb: string | KustoConnectionStringBuilder, defaultProps: IngestionProperties | null = null) {
         super(defaultProps);
-        this.streamingIngestClient = new StreamingIngestClient(engineKcsb);
-        this.queuedIngestClient = new IngestClient(dmKcsb);
+        this.streamingIngestClient = new StreamingIngestClient(engineKcsb, defaultProps);
+        this.queuedIngestClient = new IngestClient(dmKcsb, defaultProps);
     }
 
     async ingestFromStream(stream: StreamDescriptor | Readable, ingestionProperties: IngestionProperties): Promise<any> {
