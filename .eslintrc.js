1881715f553b6c7d3623990cbfea7a8b022a5025
AsafMah <asafmahlev@microsoft.com>
2022-04-12 14:17:29 +0300

    Fixed lint, don't pack test

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,10 @@ module.exports = {
         node: true,
     },
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking", "prettier"],
-    ignorePatterns: ["node_modules", "**/*.d.ts", "**/*.js"],
+    ignorePatterns: ["node_modules", "**/*.d.ts", "**/*.js", "dist"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "tsconfig.json",
+        project: ["./packages/*/tsconfig.json"],
         sourceType: "module",
     },
     plugins: ["eslint-plugin-jsdoc", "eslint-plugin-prefer-arrow", "@typescript-eslint", "header"],
