ae73266ef63119c7d9b623ed0fffe7afc5a608f7
AsafMah <asafmahlev@microsoft.com>
2022-04-24 10:40:52 +0300

    formatting

--- packages/azure-kusto-ingest/source/ingestionProperties.ts
+++ packages/azure-kusto-ingest/source/ingestionProperties.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-
 import { IngestionPropertiesValidationError } from "./errors";
 import { ColumnMapping } from "./columnMappings";
 
