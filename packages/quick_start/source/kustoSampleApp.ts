c7c02ed52efa7fb3b35c464fd7a5c171698f5150
ronmoneta <t-ronmoneta@microsoft.com>
2022-04-28 12:53:09 +0300

    Added files + setup

---
+++ packages/quick_start/source/kustoSampleApp.ts
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// import KustoClient from "azure-kusto-data/source/client";
+//
+// const client = new KustoClient("https://kusto.test.com");
+// const KustoConnectionStringBuilder = new KustoConnectionStringBuilder;
+//
+// const kcsb = KustoConnectionStringBuilder.withAadApplicationKeyAuthentication(`https://${clusterName}.kusto.windows.net`, "appid", "appkey", "authorityId");
+// const client = new KustoClient(kcsb);
+//
+// const results = await client.execute("db", "TableName | limit 1");
+// console.log(JSON.stringify(results));
+// console.log(results.primaryResults[0].toString());
+
