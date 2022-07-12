baa16ebcc9524b907a2b6fd1f03b86c174a13abc
Asaf Mahlev <asafmahlev@microsoft.com>
2022-07-12 23:41:56 +0300

    no

--- packages/azure-kusto-ingest/test/e2eTests/e2eTest.ts
+++ packages/azure-kusto-ingest/test/e2eTests/e2eTest.ts
@@ -278,7 +278,8 @@ const main = (): void => {
                     properties.setTimeout(10);
                     await queryClient.executeQuery(databaseName, `${tableName}`, properties);
                 } catch (ex: unknown) {
-                    assert.match((ex as { code: string }).code, /.*Query is expired.*/, `ex json: ${JSON.stringify(ex)}, ex: ${ex}`);
+                    assert.ok(ex instanceof Error);
+                    assert.match(ex.message, /.*Query is expired.*/, `Fail to get "Query is expired". ex json: ${JSON.stringify(ex)}, ex: ${ex}`);
                     return;
                 }
                 assert.fail(`Didn't throw executionTimeout`);
