7808b95255c632081b698653b2827c25c5053db1
t-ronmoneta <t-ronmoneta@microsoft.com>
2022-10-18 13:42:40 +0300

    query only mode fix - added nosource option. appropriate JSON fields will be added in frontend side (and changing IngestData flag to False)

--- packages/quick_start/source/SampleApp.ts
+++ packages/quick_start/source/SampleApp.ts
@@ -14,6 +14,7 @@ import { v4 as uuidv4 } from "uuid";
 enum SourceType {
     LocalFileSource = "localfilesource",
     BlobSource = "blobsource",
+    NoSource = "nosource",
 }
 
 export enum AuthenticationModeOptions {
