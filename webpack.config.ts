893f05d3b1e3cfeec846533cca3b3dfe304ca6b3
Ohad Bitton <ohbitton@microsoft.com>
2022-12-12 11:51:26 +0200

    lockfileversion 1

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import * as path from "path";
 // in case you run into any typescript error when configuring `devServer`
 import "webpack-dev-server";
