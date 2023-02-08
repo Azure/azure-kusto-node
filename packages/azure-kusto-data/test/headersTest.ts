f37153a006db0dde1622f22e4933f2db3a7d87c7
AsafMah <asafmahlev@microsoft.com>
2023-02-08 08:40:52 +0200

    Added headers alignment according to spec (#235)
    Co-authored-by: Ohad Bitton <ohbitton@microsoft.com>

---
+++ packages/azure-kusto-data/test/headersTest.ts
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import { ClientDetails } from "../src/clientDetails";
+import assert from "assert";
+
+describe("Test Headers", () => {
+    it("Should work with default tracing properties", () => {
+        const clientDetails = new ClientDetails(null, null);
+        const headers = clientDetails.getHeaders();
+        assert.strictEqual(headers["x-ms-client-version"]?.startsWith("Kusto.JavaScript.Client:"), true);
+        assert.notStrictEqual(headers["x-ms-user"], null);
+        assert.notStrictEqual(headers["x-ms-app"], null);
+    });
+
+    it("Should work with custom tracing properties", () => {
+        const clientDetails = ClientDetails.setConnectorDetails("test", "1.0", "testApp", "1.0", true, null, [["test", "test"]]);
+        const headers = clientDetails.getHeaders();
+        assert.strictEqual(headers["x-ms-client-version"]?.startsWith("Kusto.JavaScript.Client:"), true);
+        assert.strictEqual(headers["x-ms-app"], "Kusto.test:{1.0}|App.{testApp}:{1.0}|test:{test}");
+        assert.notStrictEqual(headers["x-ms-user"], "[none]");
+    });
+
+    it("Should work with no user", () => {
+        const clientDetails = ClientDetails.setConnectorDetails("test", "1.0", "testApp", "1.0", false, null, [["test", "test"]]);
+        const headers = clientDetails.getHeaders();
+        assert.strictEqual(headers["x-ms-client-version"]?.startsWith("Kusto.JavaScript.Client:"), true);
+        assert.strictEqual(headers["x-ms-app"], "Kusto.test:{1.0}|App.{testApp}:{1.0}|test:{test}");
+        assert.strictEqual(headers["x-ms-user"], "[none]");
+    });
+
+    it("Should work with no app", () => {
+        const clientDetails = ClientDetails.setConnectorDetails("test", "1.0", null, null, true, null, [["test", "test"]]);
+        const headers = clientDetails.getHeaders();
+        assert.strictEqual(headers["x-ms-client-version"]?.startsWith("Kusto.JavaScript.Client:"), true);
+        assert.strictEqual(headers["x-ms-app"]?.startsWith("Kusto.test:{1.0}|App."), true);
+        assert.notStrictEqual(headers["x-ms-user"], "[none]");
+    });
+
+    it("Should work with override user", () => {
+        const clientDetails = ClientDetails.setConnectorDetails("test", "1.0", "testApp", "1.0", true, "testUser", [["test", "test"]]);
+        const headers = clientDetails.getHeaders();
+        assert.strictEqual(headers["x-ms-client-version"]?.startsWith("Kusto.JavaScript.Client:"), true);
+        assert.strictEqual(headers["x-ms-app"], "Kusto.test:{1.0}|App.{testApp}:{1.0}|test:{test}");
+        assert.strictEqual(headers["x-ms-user"], "testUser");
+    });
+
+    it("Should work with no additional fields", () => {
+        const clientDetails = ClientDetails.setConnectorDetails("test", "1.0", "testApp", "1.0", true, null, null);
+        const headers = clientDetails.getHeaders();
+        assert.strictEqual(headers["x-ms-client-version"]?.startsWith("Kusto.JavaScript.Client:"), true);
+        assert.strictEqual(headers["x-ms-app"], "Kusto.test:{1.0}|App.{testApp}:{1.0}");
+        assert.notStrictEqual(headers["x-ms-user"], "[none]");
+    });
+});
