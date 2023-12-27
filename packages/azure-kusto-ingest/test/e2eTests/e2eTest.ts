0bb524aa331d87712698a02b90ed9b043275f104
Ohad Bitton <ohbitton@microsoft.com>
2023-12-27 15:29:13 +0200

    log successes

--- packages/azure-kusto-ingest/test/e2eTests/e2eTest.ts
+++ packages/azure-kusto-ingest/test/e2eTests/e2eTest.ts
@@ -551,7 +551,10 @@ const main = (): void => {
 
         const failures = await statusQueues.failure.pop();
         const successes = await statusQueues.success.pop();
-
+        console.log(`Successes: ${successes.length}, Failures: ${failures.length}`);
+        if (failures?.length){
+            console.log(`Failures: ${failures.map((f) => f.messageText)}`);
+        }
         return { SuccessCount: successes.length, FailureCount: failures.length };
     };
 
