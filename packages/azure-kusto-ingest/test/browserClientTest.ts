2cc9f3f137ed4f16227444d01825b6cf5d783591
Asaf Mahlev <asafmahlev@microsoft.com>
2024-12-18 10:16:21 +0200

    Fix test

--- packages/azure-kusto-ingest/test/browserClientTest.ts
+++ packages/azure-kusto-ingest/test/browserClientTest.ts
@@ -5,9 +5,7 @@
 
 import assert from "assert";
 import IngestClient from "../src/ingestClient.browser";
-import {
-    KustoConnectionStringBuilder as ConnectionStringBuilder
-} from "azure-kusto-data/src/connectionBuilder.browser";
+import { KustoConnectionStringBuilder as ConnectionStringBuilder } from "azure-kusto-data/src/connectionBuilder.browser";
 import sinon from "sinon";
 import { IngestionResult } from "../src/ingestionResult";
 import { DataFormat } from "azure-kusto-ingest";
